b6ce2a879aef2c75e6ab6eabdb5fae32e3feb905
morgane-marechal <morgane.marechal@laplateforme.io>
2024-09-04 16:45:21 +0200

    add connect class

--- Classe/Connect.js
+++ Classe/Connect.js
@@ -1,1 +1,38 @@
 //classe pour se connecter à la bdd postgres_database_version
+const { Client } = require('pg');
+require('dotenv').config();
+
+class Connect {
+    constructor() {
+        this.client = new Client({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+        });
+    }
+
+    //gestion connection
+    async connect() {
+        try {
+            await this.client.connect();
+            console.log('Connected to database');
+        } catch (error) {
+            console.error('Error connecting to database:', error);
+            throw error; // Rethrow the error to handle it in the calling code
+        }
+    }
+
+    async disconnect() {
+        try {
+            await this.client.end();
+            console.log('Disconnected from database');
+        } catch (error) {
+            console.error('Error disconnecting from database:', error);
+            throw error;
+        }
+    }
+  
+}
+
+module.exports = Connect;
